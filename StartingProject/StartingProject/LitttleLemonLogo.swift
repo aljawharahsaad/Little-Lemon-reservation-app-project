20764634fb93049ad3afbc31e298288d38012bb1
aljawharah almasoud <108049498+aljawharahsaad@users.noreply.github.com>
2024-08-18 15:33:02 +0300

    Add files via upload

---
+++ StartingProject/StartingProject/LitttleLemonLogo.swift
@@ -0,0 +1,16 @@
+import SwiftUI
+
+struct LittleLemonLogo: View {
+    var body: some View {
+        //display the logo 
+        Image("littleLemon")
+    }
+}
+
+struct LittleLemonLogo_Previews: PreviewProvider {
+    static var previews: some View {
+        LittleLemonLogo()
+    }
+}
+
+
